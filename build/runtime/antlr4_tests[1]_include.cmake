ef3598c9897cc719cd9084111781d03b5a7480f6
amukh1 <fr0g_gam3r@outlook.com>
2023-11-07 17:08:05 -0500

    :sob: organization bs

--- build/runtime/antlr4_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/workspaces/codespaces-blank/build/runtime/antlr4_tests[1]_tests.cmake")
-  include("/workspaces/codespaces-blank/build/runtime/antlr4_tests[1]_tests.cmake")
-else()
-  add_test(antlr4_tests_NOT_BUILT antlr4_tests_NOT_BUILT)
-endif()
