11fa752d35b96717710b984ea8f3e0ac4bf63e95
amukh1 <fr0g_gam3r@outlook.com>
2023-11-07 17:24:39 -0500

    .

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ sudo cp libantlr4-runtime.a /usr/local/lib
 cd ../..
 
 # g++ -std=c++17 *.cpp -o index -I/usr/local/include/antlr4-runtime -L. -lantlr4-runtime
-g++ *.cpp -o lucy -I/usr/local/include/antlr4-runtime -std=c++17 -L./antlr4-cpp-runtime-4.13.1/runtime/src -lantlr4-runtime -pthread -g -I src/ `llvm-config --cflags --libs`
-
+./compile.sh
+cp lucy /usr/local/bin
