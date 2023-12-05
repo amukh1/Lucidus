c4b43d394a9d4278eab03e0db069d33a9cec60fb
amukh1 <fr0g_gam3r@outlook.com>
2023-12-04 19:02:06 -0500

    help

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 # g++ -std=c++17 *.cpp -o index -I/usr/local/include/antlr4-runtime -L. -lantlr4-runtime
 g++ src/*.cpp -o lucy -I/usr/local/include/antlr4-runtime -std=c++17 -lantlr4-runtime -pthread -g  `llvm-config --cflags --libs`
-sudo mv lucy /usr/local/bin
+sudo cp lucy /usr/local/bin
